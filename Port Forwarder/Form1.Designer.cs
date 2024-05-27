f5f63fd258df924c3df3af48bb725744f56f5469
BerkaySevinc <berkaysevinc@hotmail.com>
2024-05-27 19:47:57 +0300

    Add project files.

---
+++ Port Forwarder/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Port_Forwarder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnDeleteForward = new System.Windows.Forms.Button();
+            this.txtPort2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtPort
+            // 
+            this.txtPort.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPort.Location = new System.Drawing.Point(62, 61);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(261, 39);
+            this.txtPort.TabIndex = 0;
+            this.txtPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnForward
+            // 
+            this.btnForward.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnForward.Location = new System.Drawing.Point(362, 47);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(147, 66);
+            this.btnForward.TabIndex = 1;
+            this.btnForward.Text = "Forward";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnDeleteForward
+            // 
+            this.btnDeleteForward.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDeleteForward.Location = new System.Drawing.Point(352, 201);
+            this.btnDeleteForward.Name = "btnDeleteForward";
+            this.btnDeleteForward.Size = new System.Drawing.Size(170, 76);
+            this.btnDeleteForward.TabIndex = 3;
+            this.btnDeleteForward.Text = "Delete Forwarded";
+            this.btnDeleteForward.UseVisualStyleBackColor = true;
+            this.btnDeleteForward.Click += new System.EventHandler(this.btnDeleteForward_Click);
+            // 
+            // txtPort2
+            // 
+            this.txtPort2.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPort2.Location = new System.Drawing.Point(62, 220);
+            this.txtPort2.Name = "txtPort2";
+            this.txtPort2.Size = new System.Drawing.Size(261, 39);
+            this.txtPort2.TabIndex = 2;
+            this.txtPort2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(22)))), ((int)(((byte)(24)))));
+            this.ClientSize = new System.Drawing.Size(593, 344);
+            this.Controls.Add(this.btnDeleteForward);
+            this.Controls.Add(this.txtPort2);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.txtPort);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Port Forwarder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnDeleteForward;
+        private System.Windows.Forms.TextBox txtPort2;
+    }
+}
+
---
+++ Port Forwarder/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mono.Nat;
+
+
+namespace Port_Forwarder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+
+
+
+        int Port;
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            string PortText = txtPort.Text.Trim();
+
+            if (!Int32.TryParse(PortText, out _)) return;
+
+            Port = Convert.ToInt32(PortText);
+
+            NatUtility.DeviceFound += ForwardPort;
+
+            NatUtility.StartDiscovery();
+        }
+
+        private void ForwardPort(object sender, DeviceEventArgs args)
+        {
+            INatDevice device = args.Device;
+
+            device.CreatePortMap(new Mapping(Protocol.Tcp, Port, Port));
+
+            MessageBox.Show(Port + ". Port Forwarded To This Local Device!");
+
+            NatUtility.DeviceFound -= ForwardPort;
+        }
+
+
+
+        private void btnDeleteForward_Click(object sender, EventArgs e)
+        {
+            string PortText = txtPort2.Text.Trim();
+
+            if (!Int32.TryParse(PortText, out _)) return;
+
+            Port = Convert.ToInt32(PortText);
+
+            NatUtility.DeviceFound += DeleteForwardedPort;
+
+            NatUtility.StartDiscovery();
+        }
+
+        private void DeleteForwardedPort(object sender, DeviceEventArgs args)
+        {
+            INatDevice device = args.Device;
+
+            device.DeletePortMap(new Mapping(Protocol.Tcp, Port, Port));
+
+            MessageBox.Show(Port + ". Forwarded Port Has Been Deleted!");
+
+            NatUtility.DeviceFound -= DeleteForwardedPort;
+        }
+
+
+    }
+}
